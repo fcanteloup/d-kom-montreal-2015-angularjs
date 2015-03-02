3418fac3b13600a5e1e53847945026c86aaae267
Frederic Canteloup <frederic.canteloup@hybris.com>
2015-03-02 16:55:03 -0500

    init step-2

--- webapp/http/js/application/console/tabs/example2Controller.js
+++ webapp/http/js/application/console/tabs/example2Controller.js
@@ -1,4 +1,23 @@
 angular.module('example2Module',[ 'commonModule', 'ngResource']).controller('example2Controller', function($scope, $rootScope, $location, $resource) {
 
+					$scope.search = {};
 
+					$scope.clearSearch = function(){
+						this.search.username = '';
+					}
+
+
+                    var restService = $resource("users");
+
+					$scope.$watch("search.username", function(username) {
+						restService.query({mask:username}).$promise.then(
+								function(users){
+									$scope.users = users;
+									$scope.userFound = users.length==1 && users[0].username==username;
+								},
+								function(error){
+									$scope.userFound = false;
+								}
+						)
+					});
 				});
