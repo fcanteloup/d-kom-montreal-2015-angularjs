99d0308c6c7d915516c8e262f2c66195f689b193
Frederic Canteloup <frederic.canteloup@hybris.com>
2015-03-02 16:48:22 -0500

    init step-1

--- webapp/http/js/application/console/tabs/example1Controller.js
+++ webapp/http/js/application/console/tabs/example1Controller.js
@@ -1,4 +1,10 @@
 angular.module('example1Module',[ 'commonModule']).controller('example1Controller', function($scope, $rootScope, $location) {
 
+    $scope.search = {};
 
-				});
+    $scope.clearSearch = function(){
+        this.search.username = 'somevalue';
+    }
+
+
+});
