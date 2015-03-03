cc2cba2e40ad62aa0b25ffb39e5b7f1d23a4dc33
Frederic Canteloup <frederic.canteloup@hybris.com>
2015-03-03 08:50:36 -0500

    init step-4

--- webapp/http/js/application/console/tabs/example4Controller.js
+++ webapp/http/js/application/console/tabs/example4Controller.js
@@ -1,14 +1,27 @@
-angular.module('example4Module',[ 'commonModule', 'ngResource']).controller('example4Controller', function($scope, $rootScope, $location, $resource, $filter) {
+angular.module('example4Module',[ 'commonModule']).controller('example4Controller', function($scope, $rootScope, $filter) {
 
-    var users = [
-        {id:"1", username:'Fred'},
-        {id:"2", username:'John'},
-        {id:"6", username:'Kate'},
-        {id:"3", username:'Paul'},
-        {id:"7", username:'Anne'},
-        {id:"4", username:'Alex'},
-        {id:"5", username:'James'}
-    ]
+					$scope.search = {};
 
+					$scope.clearSearch = function(){
+						this.search.username = '';
+					}
+					
+					
+					$scope.users = [
+						 		 	{id:"1", username:'Fred'},
+						 			{id:"2", username:'John'},
+						 			{id:"3", username:'Paul'},
+						 			{id:"4", username:'Alex'},
+						 			{id:"5", username:'James'},
+						 			{id:"6", username:'Kate'},
+						 			{id:"7", username:'Anne'}
+						 		]
+					
+					$scope.timestamp = 1417192430699;
+					$scope.formattedTimestamp = $filter('date')($scope.timestamp, "dd/MM/yyyy");
+					
+					$scope.$watch("search.username", function(username) {
+						$scope.fringed = $filter('fringes')(username, "$");
+					});
 
 				});
